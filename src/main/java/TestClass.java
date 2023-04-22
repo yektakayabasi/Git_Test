fd7a4061278f0f2a66e0137f9dae82ce92c09ab8
yektakayabasi <116421145+yektakayabasi@users.noreply.github.com>
2023-04-22 13:35:37 +0300

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Hello from the other side");
+        System.out.println("Hello from the git side");
+         System.out.println("Hello from the github side");
     }
 }
